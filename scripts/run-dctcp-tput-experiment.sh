b8bd440241a2afc7143a53f13ec43cf3001ea926
Saksham Agarwal <sakshamagarwals@live.com>
2023-08-18 21:33:36 +0000

    added dctcp script

--- scripts/run-dctcp-tput-experiment.sh
+++ scripts/run-dctcp-tput-experiment.sh
@@ -5,24 +5,22 @@ help()
     echo "Usage: run-rdma-tput-experiment
                [ -H | --home (home directory)]
                [ -S | --server (ip address of the server)]
-               [ --server_dev (RDMA device for the server, for eg., mlx5_0/mlx5_1, depends upon the interface port number and the driver)]
                [ --server_intf (interface name for the server, for eg., ens2f0)]
+               [ --num_servers (number of servers)]
                [ -C | --client (ip address of the client)]
-               [ --client_dev (RDMA device for the client, for eg., mlx5_0/mlx5_1, depends upon the interface port number and the driver)]
                [ --client_intf (interface name for the client, for eg., ens2f0)]
+               [ --num_clients (number of clients)]
                [ -E | --exp (experiment name, this name will be used to create output directories; default='rdma-test')]
-               [ -t | --txn (=read/write/send; provides what RDMA transaction to use; default='write') ]
                [ -M | --MTU (=256/512/1024/2048/4096; MTU size used;default=4096) ]
-               [ -s | --size (message size used in bytes, for eg., 65536 for 64KB message size; default=65536) ]
                [ -d | --ddio (=0/1, whether DDIO is disabled or enabled) ]
-               [ -c | --cpu (CPU to run the app on, recommended to run on NUMA node local to the NIC for maximum performance; default=0) ]
-               [ --mlc_cores (comma separated values for MLC cores, for eg., '4,8,12' for using cores 4,8 and 12. Use 'none' to skip running MLC.) ]
+               [ -c | --cpu_mask (comma separated CPU mask to run the app on, recommended to run on NUMA node local to the NIC for maximum performance; default=0) ]
+               [ --mlc_cores (comma separated values for MLC cores, for eg., '1,2,3' for using cores 1,2 and 3. Use 'none' to skip running MLC.) ]
                [ -h | --help  ]"
     exit 2
 }
 
-SHORT=H:,S:,C:,E:,t:,M:,s:,d:,c:,h
-LONG=home:,server:,client:,server_dev:,client_dev:,server_intf:,client_intf:,exp:,txn:,MTU:,size:,ddio:,cpu:,mlc_cores:,help
+SHORT=H:,S:,C:,E:,M:,d:,c:,h
+LONG=home:,server:,client:,num_servers:,num_clients:,server_intf:,client_intf:,exp:,txn:,MTU:,size:,ddio:,cpu_mask:,mlc_cores:,help
 OPTS=$(getopt -a -n run-rdma-tput-experiment --options $SHORT --longoptions $LONG -- "$@")
 
 VALID_ARGUMENTS=$# # Returns the count of arguments that are in short or long options
@@ -35,25 +33,23 @@ eval set -- "$OPTS"
 
 #default values
 exp="rdma-test"
-server="192.168.11.122"
-client="192.168.11.121"
-server_dev="mlx5_0"
-client_dev="mlx5_1"
-server_intf="ens2f0"
-client_intf="ens2f1"
-txn="write"
-size=65536
+server="192.168.10.122"
+client="192.168.10.121"
+server_intf="ens2f1"
+client_intf="ens2f0"
+num_servers=4
+num_clients=4
+init_port=3000
 ddio=0
-mtu=4096
+mtu=4000
 dur=30
-sl=0
-cpu=0
+cpu_mask="4,8,12,16"
 mlc_cores="none"
 mlc_dur=100
 num_runs=2
 home="/home/saksham"
 setup_dir=$home/hostCC/utils
-exp_dir=$home/hostCC/utils/rdma
+exp_dir=$home/hostCC/utils/tcp
 mlc_dir=$home/mlc/Linux
 
 echo -n "Enter SSH Username for client:"
@@ -82,8 +78,8 @@ do
       server="$2"
       shift 2
       ;;
-    --server_dev )
-      server_dev="$2"
+    --num_servers )
+      num_servers="$2"
       shift 2
       ;;
      --server_intf )
@@ -94,32 +90,24 @@ do
       client="$2"
       shift 2
       ;;
-    --client_dev )
-      client_dev="$2"
+    --num_clients )
+      num_clients="$2"
       shift 2
       ;;
      --client_intf )
       client_intf="$2"
       shift 2
       ;;
-    -t | --txn )
-      txn="$2"
-      shift 2
-      ;;
     -M | --MTU )
       mtu="$2"
-      shift 2
-      ;;
-    -s | --size )
-      size="$2"
       shift 2
       ;;
     -d | --ddio )
       ddio="$2"
       shift 2
       ;;
-    -c | --cpu )
-      cpu="$2"
+    -c | --cpu_mask )
+      cpu_mask="$2"
       shift 2
       ;;
     --mlc_cores )
@@ -159,14 +147,11 @@ function progress_bar() {
 
 function cleanup() {
     sudo pkill -9 -f loaded_latency
-    sudo pkill -9 -f ib_read_bw
-    sudo pkill -9 -f ib_write_bw
-    sudo pkill -9 -f ib_send_bw
-    sshpass -p $password ssh $uname@$addr "screen -ls | awk '/\(Detached\)/ { system(\"screen -S \" \$1 \" -X quit\") }'"
+    sudo pkill -9 -f iperf
+    sshpass -p $password ssh $uname@$addr 'screen -S $(screen -list | awk "/\\.client_session\t/ {print \$1}") -X quit'
+    sshpass -p $password ssh $uname@$addr 'screen -S $(screen -list | awk "/\\.logging_session\t/ {print \$1}") -X quit'
     sshpass -p $password ssh $uname@$addr 'screen -wipe'
-    sshpass -p $password ssh $uname@$addr 'sudo pkill -9 -f ib_read_bw'
-    sshpass -p $password ssh $uname@$addr 'sudo pkill -9 -f ib_write_bw'
-    sshpass -p $password ssh $uname@$addr 'sudo pkill -9 -f ib_send_bw'
+    sshpass -p $password ssh $uname@$addr 'sudo pkill -9 -f iperf'
 }
 
 
@@ -183,7 +168,7 @@ if [ "$mlc_cores" = "none" ]; then
     # Perform actions for "none" input
 else
     echo "starting MLC..."
-    $mlc_dir/mlc --loaded_latency -T -d0 -e -k$mlc_cores -j0 -b1g -t10000 -W5 &> mlc.log &
+    $mlc_dir/mlc --loaded_latency -T -d0 -e -k$mlc_cores -j0 -b1g -t10000 -W2 &> mlc.log &
     ## wait until MLC starts sending memory traffic at full rate
     echo "waiting for MLC for start..."
     progress_bar 30 1
@@ -192,18 +177,18 @@ fi
 #### setup and start servers
 echo "setting up server config..."
 cd $setup_dir
-sudo bash setup-envir.sh -i $server_intf -a $server -m $mtu -d $ddio -f 1 -r 1 -p 1
+sudo bash setup-envir.sh -i $server_intf -a $server -m $mtu -d $ddio -f 1 -r 0 -p 0 -e 1 -b 1 -o 1
 cd -
 
 echo "starting server instances..."
 cd $exp_dir
-sudo bash run-netapp-tput.sh -m server -d $server_dev -t $txn -D $(($dur * 5)) -o $exp-RUN-$j -s $size -M $mtu &
+sudo bash run-netapp-tput.sh -m server -S $num_servers -o $exp-RUN-$j -p $init_port -c $cpu_mask &
 sleep 2
 cd -
 
 #### setup and start clients
 echo "setting up and starting clients..."
-sshpass -p $password ssh $uname@$addr 'screen -dmS client_session sudo bash -c "cd '$setup_dir'; sudo bash setup-envir.sh -i '$client_intf' -a '$client' -m '$mtu' -d '$ddio' -f 1 -r 1 -p 1; cd '$exp_dir'; sudo bash run-netapp-tput.sh -m client -a '$server' -d '$client_dev' -t '$txn' -D '$(($dur * 5))' -o '$exp'-RUN-'$j' -s '$size' -M '$mtu'; exec bash"'
+sshpass -p $password ssh $uname@$addr 'screen -dmS client_session sudo bash -c "cd '$setup_dir'; sudo bash setup-envir.sh -i '$client_intf' -a '$client' -m '$mtu' -d '$ddio' -f 1 -r 0 -p 0 -e 1 -b 1 -o 1; cd '$exp_dir'; sudo bash run-netapp-tput.sh -m client -a '$server' -C '$num_clients' -S '$num_servers' -o '$exp'-RUN-'$j' -p '$init_port' -c '$cpu_mask'; exec bash"'
 
 
 #### warmup
@@ -211,14 +196,21 @@ echo "warming up..."
 progress_bar 10 1
 
 #record stats
+##start sender side logging
+echo "starting logging at client..."
+sshpass -p $password ssh $uname@$addr 'screen -dmS logging_session sudo bash -c "cd '$setup_dir'; sudo bash record-host-metrics.sh -t 1 -i '$client_intf' -o '$exp-RUN-$j' --type 0 --cpu_util 1 --retx 1 --pcie 0 --membw 0 --dur '$dur' --cores '$cpu_mask' ; exec bash"'
+
 ##start receiver side logging
 echo "starting logging at server..."
 cd $setup_dir
-sudo bash record-host-metrics.sh -t 1 -i $server_intf -o $exp-RUN-$j --bw 1 --cpu_util 1 --pcie 1 --membw 1 --iio 1 --pfc 1 --dur $dur
+sudo bash record-host-metrics.sh -t 1 -i $server_intf -o $exp-RUN-$j --type 0 --cpu_util 1 --pcie 1 --membw 1 --dur $dur --cores $cpu_mask
 echo "done logging..."
 cd -
 
-sleep $(($dur * 3))
+#transfer sender-side info back to receiver
+sshpass -p $password scp $uname@$addr:$setup_dir/reports/$exp-RUN-$j/retx.rpt $setup_dir/reports/$exp-RUN-$j/retx.rpt
+
+sleep $(($dur * 2))
 
 #post-run cleanup
 cleanup
@@ -237,22 +229,22 @@ else
     #### setup and start servers
     echo "setting up server config..."
     cd $setup_dir
-    sudo bash setup-envir.sh -i $server_intf -a $server -m $mtu -d $ddio -f 1 -r 1 -p 1
+    sudo bash setup-envir.sh -i $server_intf -a $server -m $mtu -d $ddio -f 1 -r 0 -p 0 -e 1 -b 1 -o 1
     cd -
 
     echo "starting server instances..."
     cd $exp_dir
-    sudo bash run-netapp-tput.sh -m server -d $server_dev -t $txn -D $(($dur * 5)) -o $exp-MLCRUN-$j -s $size -M $mtu &
+    sudo bash run-netapp-tput.sh -m server -S $num_servers -o $exp-MLCRUN-$j -p $init_port -c $cpu_mask &
     sleep 2
     cd -
 
     #### setup and start clients
     echo "setting up and starting clients..."
-    sshpass -p $password ssh $uname@$addr 'screen -dmS client_session sudo bash -c "cd '$setup_dir'; sudo bash setup-envir.sh -i '$client_intf' -a '$client' -m '$mtu' -d '$ddio' -f 1 -r 1 -p 1; cd '$exp_dir'; sudo bash run-netapp-tput.sh -m client -a '$server' -d '$client_dev' -t '$txn' -D '$(($dur * 5))' -o '$exp'-MLCRUN-'$j' -s '$size' -M '$mtu'; exec bash"'
+    sshpass -p $password ssh $uname@$addr 'screen -dmS client_session sudo bash -c "cd '$setup_dir'; sudo bash setup-envir.sh -i '$client_intf' -a '$client' -m '$mtu' -d '$ddio' -f 1 -r 0 -p 0 -e 1 -b 1 -o 1; cd '$exp_dir'; sudo bash run-netapp-tput.sh -m client -a '$server' -C '$num_clients' -S '$num_servers' -o '$exp'-MLCRUN-'$j' -p '$init_port' -c '$cpu_mask'; exec bash"'
 
     #### start MLC
     echo "starting MLC..."
-    $mlc_dir/mlc --loaded_latency -T -d0 -e -k$mlc_cores -j0 -b1g -t$mlc_dur -W5 &> $setup_dir/reports/$exp-MLCRUN-$j/mlc.log &
+    $mlc_dir/mlc --loaded_latency -T -d0 -e -k$mlc_cores -j0 -b1g -t$mlc_dur -W2 &> $setup_dir/reports/$exp-MLCRUN-$j/mlc.log &
     ## wait until MLC starts sending memory traffic at full rate
     sleep 30
     echo "Running MLC..."
@@ -265,9 +257,9 @@ fi
 
 #collect info from all runs
 if [ "$mlc_cores" = "none" ]; then
-    sudo python3 collect-rdma-tput-stats.py $exp $num_runs 0
+    sudo python3 collect-tput-stats.py $exp $num_runs 0
 else
-    sudo python3 collect-rdma-tput-stats.py $exp $num_runs 1
+    sudo python3 collect-tput-stats.py $exp $num_runs 1
 fi
 
 
