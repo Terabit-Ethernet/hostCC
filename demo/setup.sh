281b002e4b26a77de2ed27dee277f88e37cbc4b8
Saksham Agarwal <sakshamagarwals@live.com>
2023-08-26 20:47:28 +0000

    added setup script for dashboard

--- demo/setup.sh
+++ demo/setup.sh
@@ -1,2 +1,10 @@
 mkdir -p logs
 sudo modprobe msr
+sudo pkill -9 -f pcm
+sudo pkill -9 -f pcm
+sudo taskset -c 27 python3 app.py &
+sleep 3
+sudo taskset -c 23 python3 collect_pcie_bw.py > col_pcie.log &
+sudo taskset -c 26 python3 collect_mem_bw.py > col_membw.log &
+sudo taskset -c 22 python3 collect_iio_occ.py > col_iio.log &
+sudo taskset -c 25 python3 collect_drop_rate.py > col_drops.log &
